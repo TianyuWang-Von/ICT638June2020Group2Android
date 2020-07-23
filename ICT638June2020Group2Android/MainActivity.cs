5a26daa1038748713fb841e6be822223a8a7e8ff
Kunye Song <1811868@nzse.ac.nz>
2020-07-24 11:56:26 +1200

    test

--- ICT638June2020Group2Android/MainActivity.cs
+++ ICT638June2020Group2Android/MainActivity.cs
@@ -11,6 +11,7 @@ namespace ICT638June2020Group2Android
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
+            //test
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
