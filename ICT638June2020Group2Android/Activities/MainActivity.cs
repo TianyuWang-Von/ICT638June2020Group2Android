0d8d4861240946619f096a263e50db0bca93c901
Kunye Song <1811868@nzse.ac.nz>
2020-08-12 13:48:59 +1200

    sky layout

--- ICT638June2020Group2Android/Activities/MainActivity.cs
+++ ICT638June2020Group2Android/Activities/MainActivity.cs
@@ -13,16 +13,13 @@ namespace ICT638June2020Group2Android
         {
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
-            // Set our view from the "main" layout resource
+
             SetContentView(Resource.Layout.activity_main);
-            //Sky nmsl
-            //Sky Test Commit2
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
            
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-            //TESTSKY1
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
